80daabef7da15f689f082eef44d155e76647a85a
bulb <lemontasteicetea@proton.me>
2024-03-07 21:07:44 +0200

    Initial proof-of-concept commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import logging
+from funcs import *
+from utils import *
+from pathlib import Path
+logging.basicConfig(level=logging.NOTSET)
+
+if __name__ == "__main__":
+	run_mod("Exit Music Redux 1.1", skip_splash_screen=True)
