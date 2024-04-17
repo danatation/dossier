e6724c26f4cf4124746d3a8e421d5890b688ef84
bulb <lemontasteicetea@proton.me>
2024-04-17 17:51:46 +0300

    config support with toml

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ from pathlib import Path
 from gui import mainwindow_ui, ddms
 import sys
 
+# TODO move appdata/ folder inside of the mods game/ folder and rename it to ddms/
+# TODO basic gui with mod importing and selection and playation
+# incorporate the mod list and fix that weird bug with the merged cells
+# implement the asset viewer with rpatool and unrpyc
+
 if __name__ == '__main__':
 	# ui_path = Path.cwd() / 'src' / 'gui' / 'mainwindow.ui'
 	# compilers.compile_ui(ui_path, 'mainwindow_ui.py')
@@ -15,8 +20,9 @@ if __name__ == '__main__':
 	# app = QApplication(sys.argv)
 	# window = ddms.MainWindow()
 	# window.show()
-	# sys.exit(app.exec())
+	# sys.exit(app.exec())	
 	
-	run_mod('Exit Music Redux', skip_splash_scr=True, skip_menu=False)
-	
+	# setup_mod('Impossible')
+	run_mod('Impossible')
+
 	''' gui will come in later '''
